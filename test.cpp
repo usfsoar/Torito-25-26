73e798ef6da49fa5e2a3f527d862384d90373145
ppupy486 <ppupy486@gmail.com>
2026-01-27 02:31:35 -0500

    Beginning to transition to cpp from Python

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <libserial.h>
+
+constexpr const char* const SERIAL_PORT = "/dev/ttyTHS1";
+
+using LibSerial
+
+using namespace std;
+
+
+int main(){
+    cout<<"hello world!\n";
+    return 0;
+}
