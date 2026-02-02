9f24c8e89f660c5d886754d1ba41ca02f8b3e0ef
jay35711 <jay35710@gmail.com>
2026-02-02 11:13:51 -0500

    Add files via upload
    AT command that changes parameters for lora added by Hari

---
+++ UV Project/LoRa_timing_beta.py
@@ -0,0 +1,38 @@
+
+#Created by J. Huang 
+
+import time
+import jetson_lora_script_beta as LoRaTransceiver
+
+lora = LoRaTransceiver.LoRaTransceiver('/dev/ttyTHS1', 115200)
+
+def main():
+    #startTime = time.clock_gettime_ns(0)/1000000
+    startTime = time.time_ns()/1000000
+    lora.send_message("C","7")
+    stop1 = time.time_ns()/1000000
+    LoRAmessage  = ""
+    while(lora.ser.in_waiting>0):
+        LoRAmessage = lora.ser.readline().decode('utf-8', errors='ignore').strip()
+        print(LoRAmessage)
+    line = ""
+    while(1):
+        if(lora.ser.in_waiting > 0):
+            line = lora.ser.readline().decode('utf-8', errors='ignore').strip()
+            if line.startswith("+RCV="):
+                parts = line.split(',')
+                if len(parts) >= 3:
+                    sender_addr = parts[0].split('=')[1]
+                    msg_len = parts[1]
+                    content = parts[2]
+                if parts[2] == "C":
+                    endTime = time.time_ns(0)/1000000
+                    t1 = stop1 - startTime
+                    et = endTime - startTime
+                    print(f"Round-trip time: {t1} ms")
+                    print(f"Round-trip time: {et} ms")
+                break
+            else:
+                print(line)
+
+main()
